b319e006aceca68e578e663fa539231718b73d50
Johannes Krämer <johannes.kraemer@hotmail.com>
2022-02-15 22:53:29 +0100

    Update MovieCard with Router Link

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import { Link } from "react-router-dom";
+
+import "./movie-card.scss";
+
 export class MovieCard extends React.Component {
   render() {
-    const { movie, onMovieClick } = this.props;
+    const { movie } = this.props;
 
     return (
-      <Card>
+      <Card className='text-white bg-secondary'>
         <Card.Img variant="top" src={movie.ImagePath} />
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
-          <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+          <Link to={`/movies/${movie._id}`}>
+            <Button variant="info">Details</Button>
+          </Link>
         </Card.Body>
       </Card>
     );
