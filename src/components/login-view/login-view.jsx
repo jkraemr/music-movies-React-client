a8c41c959504e1e5507a6b95824deb56de12efe8
Johannes Krämer <johannes.kraemer@hotmail.com>
2022-02-21 10:41:10 +0100

    Update LoginView RegistrationView

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 
+import { connect } from 'react-redux';
+import { setUser } from '../../actions/actions';
+
+import { Link } from 'react-router-dom';
+
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 
 import './login-view.scss';
 
-export function LoginView(props) {
+let mapStateToProps = (state) => {
+  return { userData: state.userData };
+};
+
+function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -33,23 +44,27 @@ export function LoginView(props) {
       isReq = false;
     }
     return isReq;
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const isReq = validate();
+    // Send auth request to server
     if (isReq) {
-      // Send auth request to server
-      axios.post('https://mymusicmovies.herokuapp.com/login', {
-        Username: username,
-        Password: password
-      })
-        .then(response => {
-          const data = response.data;
-          props.onLoggedIn(data);
+      /* Send a request to the server for authentication */
+      axios
+        .post('https://mymusicmovies.herokuapp.com/login', {
+          Username: username,
+          Password: password,
         })
-        .catch(e => {
-          console.log('no such user')
+        .then((response) => {
+          const data = response.data;
+          props.setUser(data);
+          props.onLoggedIn();
+        })
+        .catch((e) => {
+          alert('No such user or password 😭 Please try again 💪');
+          console.log(e);
         });
     }
   };
@@ -58,29 +73,55 @@ export function LoginView(props) {
 
     <Card className='text-white bg-secondary'>
       <Card.Body>
-        <Card.Title>Sign in</Card.Title>
+        <Card.Title>Log into your account</Card.Title>
         <Form>
-          <Form.Group controlId="formUsername">
+          <Form.Group controlId='formUsername'>
             <Form.Label>Username:</Form.Label>
-            <Form.Control type="text" placeholder="Enter your username" value={username} onChange={e => setUsername(e.target.value)} />
-            {/* code added here to display validation error */}
-            {usernameErr && <p>{usernameErr}</p>}
+            <Form.Control type='text' value={username} placeholder='Your username' onChange={(e) => setUsername(e.target.value)}
+            />
+            {usernameErr && <Alert variant='info'>{usernameErr}</Alert>}
           </Form.Group>
 
-          <Form.Group controlId="formPassword">
+          <Form.Group controlId='formPassword'>
             <Form.Label>Password:</Form.Label>
-            <Form.Control type="password" placeholder="Enter your password" onChange={e => setPassword(e.target.value)} />
-            {/* code added here to display validation error */}
-            {passwordErr && <p>{passwordErr}</p>}
+            <Form.Control type='password' placeholder='Your password' value={password} onChange={(e) => setPassword(e.target.value)}
+            />
+            {passwordErr && <Alert variant='info'>{passwordErr}</Alert>}
           </Form.Group>
+
+          <Button variant='success' type='submit' onClick={handleSubmit}>
+            Log In
+          </Button>
 
           <br />
+          <br />
 
-          <Button variant="info" type="submit" onClick={handleSubmit}>
-            Login
-          </Button>
+          <Card.Title>No account yet?</Card.Title>
+          <Link to={`/register`}>
+            <Button variant='info' type='button'>
+              Create Account
+            </Button>
+          </Link>
         </Form>
       </Card.Body>
-    </Card>
+    </Card >
   );
 }
+
+LoginView.propTypes = {
+  userData: PropTypes.shape({
+    User: PropTypes.shape({
+      _id: PropTypes.string,
+      Username: PropTypes.string,
+      Password: PropTypes.string,
+      Email: PropTypes.string,
+      Birthday: PropTypes.string,
+      FavoriteMovies: PropTypes.array,
+    }),
+    token: PropTypes.string,
+  }),
+  setUser: PropTypes.func,
+  onLoggedIn: PropTypes.func,
+};
+
+export default connect(mapStateToProps, { setUser })(LoginView);
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+import { Link } from 'react-router-dom';
+
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 
 import './registration-view.scss';
 
-export function RegistrationView(props) {
+export function RegistrationView() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
-  // const [passwordrep, setPasswordRep] = useState("");
   const [email, setEmail] = useState('');
   const [birthday, setBirthday] = useState('');
 
@@ -25,28 +26,34 @@ export function RegistrationView(props) {
   const validate = () => {
     let isReq = true;
     if (!username) {
-      setUsernameErr('Please enter a username');
+      setUsernameErr('Username required');
       isReq = false;
     } else if (username.length < 4) {
-      setUsernameErr('Please enter at least 4 characters');
+      setUsernameErr('Username must contain at least 4 characters');
       isReq = false;
+    } else {
+      setUsernameErr('');
     }
     if (!password) {
       setPasswordErr('Password required');
       isReq = false;
     } else if (password.length < 8) {
-      setPasswordErr('Password must be at least 8 characters long');
+      setPasswordErr('Password must contain at least 8 characters');
       isReq = false;
+    } else {
+      setPasswordErr('');
     }
     if (!email) {
-      setEmailErr('Please enter an email address');
+      setEmailErr('Email required');
       isReq = false;
     } else if (email.indexOf('@') === -1) {
       setEmailErr('Please enter a valid email address');
       isReq = false;
+    } else {
+      setEmailErr('');
     }
     return isReq;
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -57,17 +64,16 @@ export function RegistrationView(props) {
         Username: username,
         Password: password,
         Email: email,
-        Birthday: birthday
+        Birthday: birthday,
       })
-        .then(response => {
+        .then((response) => {
           const data = response.data;
-          console.log(data);
           alert('Registration successful! Please log in.');
           window.open('/', '_self'); // opening window in current tab
         })
-        .catch(response => {
+        .catch((response) => {
           console.error(response);
-          alert('Unable to register');
+          alert('Something went wrong. Please try again.');
         });
     }
   };
@@ -76,49 +82,57 @@ export function RegistrationView(props) {
 
     <Card className='text-white bg-secondary'>
       <Card.Body>
-        <Card.Title>Create account</Card.Title>
+        <Card.Title>Create Account</Card.Title>
         <Form>
           <Form.Group controlId='formUsername'>
             <Form.Label>Username:</Form.Label>
-            <Form.Control type='text' value={username} placeholder='4 characters required' onChange={e => setUsername(e.target.value)} />
-            {usernameErr && <p>{usernameErr}</p>}
+            <Form.Control type='text' value={username} placeholder='Enter a username' onChange={(e) => setUsername(e.target.value)}
+            />
+            {usernameErr && <Alert variant='info'>{usernameErr}</Alert>}
           </Form.Group>
 
           <Form.Group controlId='formPassword'>
             <Form.Label>Password:</Form.Label>
-            <Form.Control type='password' value={password} placeholder='8 characters incl. 1 number required' onChange={e => setPassword(e.target.value)} />
-            {passwordErr && <p>{passwordErr}</p>}
+            <Form.Control type='password' value={password} onChange={(e) => setPassword(e.target.value)}
+              required minLength='8'
+              placeholder='Enter a password (8 characters required)'
+            />
+            {passwordErr && <Alert variant='info'>{passwordErr}</Alert>}
           </Form.Group>
 
-          <Form.Group controlId='formEmail'>
+          <Form.Group controlId='Email'>
             <Form.Label>Email:</Form.Label>
-            <Form.Control type='email' value={email} onChange={e => setEmail(e.target.value)} />
-            {emailErr && <p>{emailErr}</p>}
+            <Form.Control type='email' value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='Enter your email' required
+            />
+            {emailErr && <Alert variant='info'>{emailErr}</Alert>}
           </Form.Group>
 
-          <Form.Group controlId='formBirthday'>
-            <Form.Label>Birthday:</Form.Label>
-            <Form.Control type='date' value={birthday} onChange={e => setBirthday(e.target.value)} />
-            {birthdayErr && <p>{birthdayErr}</p>}
+          <Form.Group controlId='updateBirthday'>
+            <Form.Label>Birthday</Form.Label>
+            <Form.Control type='date' value={birthday}
+              onChange={(e) => setBirthday(e.target.value)}
+            />
           </Form.Group>
 
           <br />
 
-          <Button variant='info' type='submit' onClick={handleSubmit}>
-            Register</Button>
-        </Form >
+          <Button type='submit' variant='success' onClick={handleSubmit}>
+            Create Account
+          </Button>
+
+          <br />
+          <br />
+
+          <Card.Title>Already have an account?</Card.Title>
+          <Link to={'/'}>
+            <Button variant='info' type='button'>
+              Go to Login
+            </Button>
+          </Link>
+        </Form>
       </Card.Body>
     </Card>
   );
 }
-
-// // propTypes
-// RegistrationView.propTypes = {
-//   register: PropTypes.shape({
-//     Username: PropTypes.string,
-//     Password: PropTypes.string,
-//     Email: PropTypes.string,
-//     // Birthday: PropTypes.string,
-//     onCLick: PropTypes.func
-//   })
-// };
