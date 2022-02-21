701dbdbd5072adac8b76f666ac93c5ae716b42d1
Johannes Krämer <johannes.kraemer@hotmail.com>
2022-02-21 10:43:19 +0100

    Add VisibilityFilterInput live search component

---
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -0,0 +1,18 @@
+// visibility-filter-input.jsx
+
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import { connect } from 'react-redux';
+import { setFilter } from '../../actions/actions';
+
+function VisibilityFilterInput(props) {
+  return (
+    <Form.Control
+      onChange={(e) => props.setFilter(e.target.value)}
+      value={props.visibilityFilter}
+      placeholder='Search'
+    />
+  );
+}
+
+export default connect(null, { setFilter })(VisibilityFilterInput);
